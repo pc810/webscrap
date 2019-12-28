479442228bad2797522bf634841aa331dcfe3aa4
swarpatel23 <smp1613@gmail.com>
2019-12-28 19:28:47 +0530

    data cleaning script added

---
+++ cleaning/clean.js
@@ -0,0 +1,112 @@
+const fs = require('fs');
+const tm = require('text-miner');
+const path = require('path');
+
+const directoryPath = "../puppet/bollymovie1";
+const savebase = "cleanedReview/";
+var filerev = [];
+var data,obj,filteredreview;
+fs.readdir(directoryPath, function (err, files) {
+
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+    let base = "../puppet/bollymovie/";
+    let revtext = [];
+    files.forEach(function (file) {
+
+        let filepath = base + file;
+        //console.log(filepath);
+        if (filepath.endsWith('.json')) {
+            let rawdata = fs.readFileSync(filepath);
+            let reviews = JSON.parse(rawdata);
+            for(var k in reviews) {
+                //console.log(k,reviews[k]);
+                reviews[k].review=cleanEmojies(reviews[k].review);
+                revtext.push(reviews[k].review);
+             }
+             var corpus = new tm.Corpus(revtext);
+            revtext = [];
+             filteredreview = corpus.trim().toLower().clean().removeInterpunctuation().removeNewlines().removeDigits().removeWords( tm.STOPWORDS.EN ).stem("Porter");
+             //var terms = new tm.DocumentTermMatrix(filteredreview);
+             //console.log(filteredreview);
+             var fileSavePath = savebase + file;
+             
+             data = JSON.stringify(filteredreview);
+             obj = JSON.parse(data);
+             filteredreview = null;
+             //console.log(obj);
+             //console.log(filerev);
+             
+             for(var k in obj.documents)
+             {
+                let  json = {}
+                 //console.log(obj.documents[k].text);
+                 json.review = obj.documents[k].text;
+                 json.label = 0;
+                 //console.log(json);
+                
+                 
+                 filerev.push(json);
+                 //json = {};
+             }
+             obj=null;
+             
+             //console.log(filerev);
+
+             rjson = JSON.stringify(filerev);
+             
+
+             console.log(rjson);
+             
+             if (rjson.length != 0) {
+                 console.log(fileSavePath);
+                fs.writeFileSync(fileSavePath, rjson, 'utf8', function (err, data) {
+                  if (err) {
+                    console.log("eror while creating review file");
+                  }
+                  else{
+                      //console.log("hi");
+                  }
+                });
+              }
+              //filerev.length = 0;
+              filerev = [];
+              obj = null;
+              data = null;
+              
+             
+             //console.log(terms.findFreqTerms(5));
+            //console.log(revtext);
+            
+
+        }
+    });
+});
+
+function cleanEmojies(rev)
+{
+    rev = rev.replace(/😚/g, "EMO_POS ");
+    rev = rev.replace(/👌/g, "EMO_POS ");
+    rev = rev.replace(/😍/g, "EMO_POS ");
+    rev = rev.replace(/👍/g, "EMO_POS ");
+    rev = rev.replace(/😂/g, "EMO_POS ");
+    rev = rev.replace(/😇/g, "EMO_POS ");
+    rev = rev.replace(/😎/g, "EMO_POS ");
+    rev = rev.replace(/🤣/g, "EMO_POS ");
+    rev = rev.replace(/🤩/g, "EMO_POS ");
+    rev = rev.replace(/🥰/g, "EMO_POS ");
+
+    rev = rev.replace(/😕/g, "EMO_NEG ");
+    rev = rev.replace(/😟/g, "EMO_NEG ");
+    rev = rev.replace(/😭/g, "EMO_NEG ");
+    rev = rev.replace(/😩/g, "EMO_NEG ");
+    rev = rev.replace(/😫/g, "EMO_NEG ");
+    rev = rev.replace(/🥱/g, "EMO_NEG ");
+    rev = rev.replace(/😤/g, "EMO_NEG ");
+    rev = rev.replace(/😡/g, "EMO_NEG ");
+    rev = rev.replace(/😠/g, "EMO_NEG ");
+
+    //rev = rev.replace(/\\'g/, "");
+    return rev;
+}
--- puppet/scrape.js
+++ puppet/scrape.js
@@ -1,33 +1,30 @@
 const puppeteer = require("puppeteer");
 const chalk = require("chalk");
 const fs = require("fs");
-const json2csv = require('json2csv').parse;
-const select = require ('puppeteer-select');
-const readline = require('readline');
+var tm = require( 'text-miner' );
 
 // MY OCD of colorful console.logs for debugging... IT HELPS
 const error = chalk.bold.red;
 const success = chalk.keyword("green");
-var urls=[];
+var urls = [];
 var lineReader = require('readline').createInterface({
   input: require('fs').createReadStream('BollywoodMovieDetail.csv')
 });
 
 lineReader.on('line', function (line) {
   var s = line.split(',"');
-      if(s[1]!=undefined)
-      {
-          s1 = s[1].split('"');
-          console.log(s1[0]);
-          urls.push(s1[0]);
-       }
+  if (s[1] != undefined) {
+    s1 = s[1].split('"');
+    console.log(s1[0]);
+    urls.push(s1[0]);
+  }
 });
 // stream = fs.open('moviename.csv', 'r',function(err,data)
 // {
 //   line = data.readLine();
 //   while(line) {
 //     //casper.echo(line);
-    
+
 //     line = stream.readLine();
 //     var s = line.split(',"');
 //     if(s[1]!=undefined)
@@ -36,11 +33,11 @@ lineReader.on('line', function (line) {
 //         console.log(s1[0]);
 //         urls.pish(s1[0]);
 //     }
-  
+
 //   }
-   
+
 // });
-var movieq,dir,loc,url,base;
+var movieq, dir, loc, url, base, page;
 (async () => {
   try {
     // open the headless browser
@@ -48,115 +45,110 @@ var movieq,dir,loc,url,base;
     // open a new page
     for (let i = 0; i < urls.length; i++) {
       url = urls[i];
-      base="https://www.google.com/search?q=";
-        dir = "bollymovie/"
-        movieq = base + url + " movie";
-        loc = dir + url +".json";
-        //console.log(loc);
+      base = "https://www.google.com/search?q=";
+      dir = "bollymovie1/"
+      movieq = base + url + " movie";
+      loc = dir + url + ".json";
+      //console.log(loc);
 
-        
-    var page = await browser.newPage();
-    // enter url in page
-    console.log(movieq);
-    await page.goto(`${movieq}`);
-    await page.waitFor(5000);
-    //page.waitForNavigation({ waitUntil: 'networkidle2' });
-    await page.evaluate(() => { if(document.querySelector('span.aKcFGd')!=undefined)
-    {
-      document.querySelector('span.aKcFGd').click();
 
-     
-      
-      
-    }
-  });
-  await page.waitFor(3000);
-    //const element = await select(page).getElement(`span.aKcFGd`);
-    await page.screenshot({path: 'buddy-screenshot.png'});
-    //console.log(element);
-    // if(element!=null)
-    // {
-    //     console.log(element.innerHTML);
-    //     await element.click();
-    //     await navigationPromise;
-    //     await page.screenshot({path: 'buddy-screenshot.png'});
-        
-    // }
-    // else
-    // {
-    //   continue;
-    // }
-    
-
-    // await page.evaluate(() => {
-    //         var example = document.querySelectorAll(`pre.T7nuU`);
-    //         console.log(example.length);
-    //         for(var i=0;i<example.length;i++)
-    //         {
-    //             console.log(example[i].text+"\n");
-    //             fs.write("movie/d3.csv", example[i].text+"\n", 'a');
-    //         }
-            
-        
-    // });
-    //page.waitForNavigation({ waitUntil: 'networkidle2' });
-
-  //   await page.evaluate(async () => {
-  //     await new Promise((resolve, reject) => {
-  //         var totalHeight = 0;
-  //         var distance = 10000;
-  //         var timer = setInterval(() => {
-  //             var scrollHeight = document.querySelector('div.U1vjCc').scrollHeight;
-  //             document.querySelector('div.U1vjCc').scrollBy(0, distance);
-  //             totalHeight += distance;
-
-  //             if(totalHeight >= scrollHeight){
-  //                 clearInterval(timer);
-  //                 resolve();
-  //             }
-  //         }, 100);
-  //     });
-  // });
-    let reviewdata = await page.evaluate(() => {
-      let reviews = [];
-      // get the hotel elements
-      let review = document.querySelectorAll('pre.T7nuU');
-      // get the hotel data
-      var j=1001;
-      review.forEach((rev) => {
-          let revJson = {};
-          loc = "movie/"+j+".csv";
-          j++;
-          console.log(loc);
-          try {
-              revJson.review = rev.innerText;
-              revJson.label = 0;
-              //fs.write(loc, rev.innerText+"\n", 'a');
-          }
-          catch (exception){
-
-          }
-          reviews.push(revJson);
-
+      page = await browser.newPage();
+      // enter url in page
+      console.log(movieq);
+      await page.goto(`${movieq}`);
+      //await page.waitFor(5000);
+      //page.waitForNavigation({ waitUntil: 'networkidle2' });
+      await page.evaluate(() => {
+        if (document.querySelector('span.aKcFGd') != undefined) {
+          document.querySelector('span.aKcFGd').click();
+        }
       });
-      return reviews;
-  });
-//   const fields = [
-//     'review',
-//     'label'
-// ];
-// let csv = json2csv({ data: reviewdata, fields });
-// fs.writeFile(loc, csv);
-  json = JSON.stringify(reviewdata);
-  fs.writeFile(loc, json, 'utf8',function(err,data)
-  {
+      await page.waitFor(3000);
+      //const element = await select(page).getElement(`span.aKcFGd`);
+      //await page.screenshot({ path: 'buddy-screenshot.png' });
+      
 
-  }); 
-  console.dir(reviewdata);
-  page.close();
-  //await page.goto(`${url}`);
-  //await page.waitForNavigation({ waitUntil: 'networkidle2' });
-}
+      await page.evaluate(() => {
+        if (document.querySelector('div.U1vjCc') != null) {
+          document.querySelector('div.U1vjCc').scrollBy(0, 6000);
+        }
+      });
+      await page.waitFor(500);
+      await page.evaluate(() => {
+        if (document.querySelector('div.U1vjCc') != null) {
+          document.querySelector('div.U1vjCc').scrollBy(0, 6000);
+        }
+      });
+      await page.waitFor(500);
+      await page.evaluate(() => {
+        if (document.querySelector('div.U1vjCc') != null) {
+          document.querySelector('div.U1vjCc').scrollBy(0, 6000);
+        }
+      });
+      await page.waitFor(500);
+      await page.evaluate(() => {
+        if (document.querySelector('div.U1vjCc') != null) {
+          document.querySelector('div.U1vjCc').scrollBy(0, 6000);
+        }
+      });
+      await page.waitFor(500);
+      let reviewdata = await page.evaluate(() => {
+
+        let reviews = [];
+        let revtext = [];
+        let review = document.querySelectorAll('pre.T7nuU');
+        review.forEach((rev) => {
+          //var my_corpus = new tm.Corpus([]);
+          
+          let revJson = {};
+          if(!rev.innerText.toLowerCase().endsWith("...more"))
+          {
+          //console.log(loc);
+          try {
+            
+            //my_corpus.addDoc(rev.innerText);
+            //my_corpus.toLower().clean().removeInterpunctuation().removeNewlines().removeDigits().stem("Porter");
+            //console.log(my_corpus);
+            revJson.review = rev.innerText.toLowerCase();
+            revJson.label = 0;
+            //revtext.push(rev.innerText);
+           
+            //fs.write(loc, rev.innerText+"\n", 'a');
+          }
+          catch (exception) {
+
+          }
+          
+          reviews.push(revJson);
+        }
+        });
+        console.log(revtext);
+        return reviews;
+      });
+      //   const fields = [
+      //     'review',
+      //     'label'
+      // ];
+      // let csv = json2csv({ data: reviewdata, fields });
+      // fs.writeFile(loc, csv);
+      json = JSON.stringify(reviewdata);
+      console.log(json.length);
+      if (json.length != 0) {
+        fs.writeFileSync(loc, json, 'utf8', function (err, data) {
+          if (err) {
+            console.log("eror while creating review file");
+          }
+        });
+
+      }
+      console.dir(reviewdata);
+      //console.log(revtext);
+      console.dir(reviewdata.length);
+
+      page.close();
+      //await page.goto(`${url}`);
+      //await page.waitForNavigation({ waitUntil: 'networkidle2' });
+    }
     debugger;
     // console.log(news);
     await browser.close();
